6b0b787866a64296796f7d04a46add0b1e7f1e0f
LDK <dlehdrl115@naver.com>
2022-07-22 16:52:53 +0900

    Initial Commit

---
+++ TamagotchiGameProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TamagotchiGameProject
+//
+//  Created by 이동기 on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
